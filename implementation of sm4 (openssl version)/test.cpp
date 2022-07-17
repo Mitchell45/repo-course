e6bf1ada762a9b7fbd7ae57f13d8ec848dabfde0
Mitchell45 <74776018+Mitchell45@users.noreply.github.com>
2022-07-17 10:16:14 +0800

    Create test.cpp

---
+++ implementation of sm4 (openssl version)/test.cpp
@@ -0,0 +1,18 @@
+#include<sm4.h>
+
+int main() {
+  int ret=0;
+	unsigned char* out = new unsigned char[outlen];
+	printf("key:");
+	print(key, 16);
+	printf("in:");
+	print(in, outlen);
+	int begin = clock();
+    ret = Ms4_openssl(in, outlen, out);
+	printf("openssl_time:\t%.3fs\n", (double)(clock() - begin) / CLOCKS_PER_SEC);
+	if (1 != ret)
+		printf("err!");
+	printf("openssl_out:");
+	print(out, outlen);
+	return 0;
+}
