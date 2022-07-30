0022b6a6d9e3282c4bd6f2c1aae397bfcf67dd80
Mitchell45 <74776018+Mitchell45@users.noreply.github.com>
2022-07-30 13:29:06 +0800

    Create openssl_sha256.h

---
+++ implementation of sha256 (openssl version)/openssl_sha256.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <iostream>
+#include "openssl/evp.h"
+using namespace std;
+
+int sm3_hash(char* plain, size_t len, unsigned char* hash, unsigned int* hash_len)
+{
+	EVP_MD_CTX* md_ctx;
+	const EVP_MD* md;
+	md = EVP_sha256();//创建sha256对象
+	md_ctx = EVP_MD_CTX_new();
+	EVP_DigestInit_ex(md_ctx, md, NULL);
+	EVP_DigestUpdate(md_ctx, plain, len);
+	EVP_DigestFinal_ex(md_ctx, hash, hash_len);
+	EVP_MD_CTX_free(md_ctx);
+	return 0;
+}
